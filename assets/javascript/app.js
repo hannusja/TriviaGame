8fe0dc59b79ff68e9eb3d14eb3fb79d480e26f11
unknown <hannahuseva@gmail.com>
2019-05-04 23:21:50 -0500

    html and css acceptable. Logic: timer works, stop/start works, working on counting hits and misses

---
+++ assets/javascript/app.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+
+    document.getElementById("main").style.visibility = "hidden"
+    document.getElementById("endscreen").style.visibility = "hidden"
+
+    var number = 60
+    var intervalId
+    var rights=0
+    var wrongs=0
+    var misses=0
+
+    $("#start").on("click", start)
+    $("#done").on("click", stop)
+    
+
+    function start(){
+        document.getElementById("main").style.visibility = "visible"
+        document.getElementById("introscreen").style.visibility = "hidden"
+        run()
+    }
+    
+    function run() {
+        clearInterval(intervalId)
+        intervalId = setInterval(decrement, 1000)
+    }
+
+    function decrement() {
+        number--
+        $("#time").html("Time remaining: "+ number)
+            if (number === 0) {
+            stop();
+            alert("Time Up!");
+        }
+    }   
+
+    function stop() {
+        clearInterval(intervalId);
+        document.getElementById("endscreen").style.visibility = "visible"
+        document.getElementById("main").style.visibility = "hidden"
+        $("#rights").append(rights)
+        $("#wrongs").append(wrongs)
+        $("#misses").append(misses)
+    }
+ 
+// Check #x
+//$(".hit").prop( "checked", true );
+ 
+// Uncheck #x
+//$( "#x" ).prop( "checked", false );
+var chosen1 = $('input:radio[name=question1]:checked').val()
+console.log(chosen1)
+if(chosen1!=undefined){
+    if(chosen1==hit)
+    rights++
+    else{
+        wrongs++
+    }
+}
+
+else{
+    misses++
+}
+
+var chosen2 = $('input:radio[name=question2]:checked').val()
+
+if(chosen2!=undefined){
+    if(chosen2="hit")
+    rights++
+    else{
+        wrongs++
+    }
+}
+
+else{
+    misses++
+}
+
+var chosen3 = $('input:radio[name=question3]:checked').val()
+
+if(chosen3!=undefined){
+    if(chosen3=="hit")
+    rights++
+    else{
+        wrongs++
+    }
+}
+
+else{
+    misses++
+}
+var chosen4 = $('input:radio[name=question4]:checked').val()
+
+if(chosen4!=undefined){
+    if(chosen4=="hit")
+    rights++
+    else{
+        wrongs++
+    }
+}
+
+else{
+    misses++
+}
+var chosen5 = $('input:radio[name=question5]:checked').val()
+
+if(chosen5!=undefined){
+    if(chosen5=="hit")
+    rights++
+    else{
+        wrongs++
+    }
+}
+
+else{
+    misses++
+}
+
+
+})
